61a1a61950a891ab24c64c225172f42117f1b7cc
Bedros Pamboukian <pastrikk00@gmail.com>
2026-01-11 05:34:49 +0400

    Spinner & realtime status

--- go.mod
+++ go.mod
@@ -5,10 +5,18 @@ go 1.24
 toolchain go1.24.11
 
 require (
+	github.com/dunglas/httpsfv v1.0.2 // indirect
+	github.com/quic-go/masque-go v0.3.0 // indirect
 	github.com/quic-go/qpack v0.6.0 // indirect
 	github.com/quic-go/quic-go v0.58.0 // indirect
+	github.com/yarlson/pin v0.9.1 // indirect
+	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
+	go.uber.org/mock v0.5.2 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/mod v0.27.0 // indirect
 	golang.org/x/net v0.43.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/tools v0.36.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"context"
 	utils "fireblazer/utils"
 	"flag"
+	"fmt"
 	"log"
 	"slices"
 	"strings"
 	"sync"
+
+	"github.com/yarlson/pin"
 )
 
 var key = flag.String("apiKey", "", "API key to scan")
 var dangerouslySkipVerification = flag.Bool("dangerouslySkipVerification", false, "Skip API key verification")
+var outputFormat = flag.String("outputFormat", "interactive", "Output format (interactive|text|json|yaml)")
+var outputDetails = flag.String("outputDetails", "full", "Comma delimited list of what to include in the details (description|title|name). Comma delimited.")
+var isInteractive = *outputFormat == "interactive" || *outputFormat == ""
 
 type APIDetails struct {
 	Description string
@@ -40,15 +47,22 @@ func main() {
 
 	wg.Add(1)
 
+	discoveryFailed := 0
 	go func() {
 		var err error
 		discoveryEndpoints, err := utils.GetDiscoveryEndpoints()
 
 		if err != nil {
-			log.Printf("Failed to get discovery endpoints: %v", err)
+			if isInteractive || *outputFormat == "text" {
+				log.Printf("Failed to get discovery endpoints: %v", err)
+				discoveryFailed++
+			}
 		}
 
 		for _, endpoint := range discoveryEndpoints {
+
+			// utils.PreresolveHost(endpoint.DiscoveryRestUrl)
+
 			gapiServices = append(gapiServices, Service{
 				CleanName:    endpoint.Name,
 				DiscoveryUrl: endpoint.DiscoveryRestUrl,
@@ -62,13 +76,21 @@ func main() {
 
 		gapiEndpoints, err := utils.GetEndpointsFromGapis()
 		if err != nil {
-			log.Printf("Failed to get supplementary endpoints from Github: %v", err)
+			if isInteractive || *outputFormat == "text" {
+				log.Printf("Failed to get supplementary endpoints from Github: %v", err)
+				discoveryFailed++
+			} else if discoveryFailed > 0 {
+				// TODO: Local-first approach for endpoints
+				log.Fatal("Both primary and supplementary endpoint retrieval failed, terminating.")
+			}
 		}
 
 		for _, endpoint := range gapiEndpoints {
 			cleanName := strings.Split(endpoint.Host, ".")[0]
 
 			discoveryUrl := "https://" + endpoint.Host + "/$discovery/rest"
+			// utils.PreresolveHost(discoveryUrl) // it gets stripped down again later - its fine. Rather not have 2 functions to resolve.
+
 			if serviceDetailMap[cleanName].Title == "" {
 				serviceDetailMap[cleanName] = APIDetails{
 					Description: endpoint.Description,
@@ -91,7 +113,9 @@ func main() {
 		}
 	}
 	if *dangerouslySkipVerification {
-		log.Println("Skipping API key verification.")
+		if isInteractive || *outputFormat == "text" {
+			log.Println("Skipping API key verification.")
+		}
 	} else if valid, err := utils.TestKeyValidity(*key); !valid {
 		log.Fatalf(`Invalid API key. 
 If you're sure the key is valid, use the --dangerouslySkipVerification flag [fireblazer --dangerouslySkipVerification AIza-KeYHere]
@@ -102,35 +126,61 @@ And submit an issue at https://github.com/bedros-p/fireblazer - include this err
 ----
 `, err)
 	} else {
-		log.Println("Valid API key, proceeding.")
+		if isInteractive || *outputFormat == "text" {
+			log.Println("Valid API key, proceeding.")
+		}
 	}
 
 	// If valid go ahead and enumerate all enabled services
 
 	// Collect results and process them
 	wg.Wait()
-	log.Println("Successfully retrieved discovery endpoints.")
+	scanPin := pin.New("Scanning...")
+
+	if isInteractive || *outputFormat == "text" {
+		log.Printf("Successfully retrieved %d discovery endpoints - %d endpoint sources failed.", len(gapiServices), discoveryFailed)
+		if discoveryFailed > 0 { // TODO: Local-first approach for endpoints
+			log.Println("A number of endpoint sources failed.\n If there's a consistently failing source and the following domains aren't blocked, raise an issue at https://github.com/bedros-p/fireblazer :")
+			log.Println("- www.googleapis.com \n- raw.githubusercontent.com")
+		}
+		if isInteractive {
+			cancel := scanPin.Start(context.Background())
+			defer cancel()
+		}
+	}
+
 	failCount := 0
 
 	discoveryWg := sync.WaitGroup{}
 	foundServices := make([]string, 0)
+	rem := len(gapiServices)
+	foundCount := 0 // idw to repeatedly check the length of foundServices
 
 	for _, item := range gapiServices {
 		discoveryWg.Add(1)
 		go func(item Service) {
+			// 8 Services in scope. %d left...
+			// baseMessage :=
 			defer discoveryWg.Done()
 			// log.Printf("Testing %v", item)
 			if valid, err := utils.TestKeyServicePair(*key, item.DiscoveryUrl); valid {
+				foundCount++
 				foundServices = append(foundServices, item.CleanName)
+
 				// log.Printf("Found discovery endpoint: %s", item)
 			} else if err != nil {
 				log.Printf("Error testing discovery endpoint %s: %v", item, err)
 				failCount++
 			}
+
+			rem--
+			scanPin.UpdateMessage(fmt.Sprintf("%d services in scope. Scanning %d more...", foundCount, rem))
+
 		}(item)
 		// time.Sleep(1 * time.Millisecond) // slight delay to avoid overwhelming the client. QUIC seems to cope and burn with no delay.
 	}
 	discoveryWg.Wait()
+	scanPin.Stop("Scan complete!")
 	log.Println("APIs available to this API key:")
 
 	for _, service := range foundServices {
